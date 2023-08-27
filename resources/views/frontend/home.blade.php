347ed849a8189c5ad51751d3e32e0abf6d415cc3
Sulav Shrestha <sulavstha2629@gmail.com>
2023-08-27 10:12:50 +0545

    Fixed Footer and Changed Color.

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,77 +1,97 @@
 @extends('layouts.app')
 @section('content')
-<div class="first-container">
-    <div class="detel">
-      <h5>Hello, my name is </h5>
-        <h1>Shraddha <span>Shrestha </span></h1>
-        <h6 class="home-bio">
-          Welcome to my portfolio 
-          <p> 
-          <br/>  
-          A showcase of my journey as a CEO in the insurance and real estate industries, as well as my engaging role as a podcaster. 
-          </p>
-          <p>
-          Explore my world where business, properties, and podcasts converge to create a unique tapestry of experiences.
-          </p>
-        </h6>
-        <a href="#" class="btn btn-outline-orange">View More </a>
-        <br>
-        <a href="#" class="btn btn-outline-secondary">Contact Me </a>
-        <div class="social-media-links mt-5 text-center">
-            <a href="https://www.facebook.com" class="social-link mr-5"><i class="fab fa-facebook-f fa-lg" style="color:orange"></i></a>
-            <a href="https://www.twitter.com" class="social-link mr-5"><i class="fab fa-twitter fa-lg" style="color:orange"></i></a>
-            <a href="https://www.instagram.com" class="social-link mr-5"><i class="fab fa-instagram fa-lg" style="color:orange"></i></a>
+    <section class="home">
+        <div class="detel">
+            <h5>Hello, my name is </h5>
+            <h1>Shraddha <span>Shrestha </span></h1>
+            <h6 class="home-bio">
+                Welcome to my portfolio
+                <p>
+                    <br/>
+                    A showcase of my journey as a CEO in the insurance and real estate industries, as well as my
+                    engaging role as a podcaster.
+                </p>
+                <p>
+                    Explore my world where business, properties, and podcasts converge to create a unique tapestry of
+                    experiences.
+                </p>
+            </h6>
+            <a href="#" class="btn btn-outline-orange">View More </a>
+            <br>
+            <a href="#" class="btn btn-outline-secondary">Contact Me </a>
+            <div class="social-media-links mt-5 text-center">
+                <a href="https://www.facebook.com" class="social-link mr-5"><i class="fab fa-facebook-f fa-lg"
+                                                                               style="color:orange"></i></a>
+                <a href="https://www.twitter.com" class="social-link mr-5"><i class="fab fa-twitter fa-lg"
+                                                                              style="color:orange"></i></a>
+                <a href="https://www.instagram.com" class="social-link mr-5"><i class="fab fa-instagram fa-lg"
+                                                                                style="color:orange"></i></a>
+            </div>
         </div>
-    </div>
-    <div class="images">
-      <img src="{{ asset('images/shraddha-home.png') }}" alt="profile image" class="girl">
-    </div>
-</div>
-<div class="services">
-  <div class="max-width">
-    <h1>Our Services</h1>
-    <div class="content">
-    <div class="card">
-      <div class="box">
-        <i class="fa fa-user"></i>
-        <h3>Service 1</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, hic error. At, cupiditate inventore. Non, ullam quibusdam. Alias repudiandae quidem numquam temporibus dolorum ab accusantium, accusamus, totam perspiciatis voluptatum consequatur.</p>
-      </div>
-    </div>
-    <div class="card">
-      <div class="box">
-        <i class="fa fa-handshake"></i>
-        <h3>Service 2</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, hic error. At, cupiditate inventore. Non, ullam quibusdam. Alias repudiandae quidem numquam temporibus dolorum ab accusantium, accusamus, totam perspiciatis voluptatum consequatur.</p>
-      </div>
-    </div>
-    <div class="card">
-      <div class="box">
-        <i class="fa fa-building"></i>
-        <h3>Service 3</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, hic error. At, cupiditate inventore. Non, ullam quibusdam. Alias repudiandae quidem numquam temporibus dolorum ab accusantium, accusamus, totam perspiciatis voluptatum consequatur.</p>
-      </div>
-    </div>
-    </div>
-  </div>
+        <div class="images">
+            <img src="{{ asset('images/shraddha-home.png') }}" alt="profile image" class="girl">
+        </div>
+    </section>
+    <section class="section-container">
+        <div class="max-width">
+            <h1>Our Services</h1>
+            <div class="content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fa fa-user"></i>
+                        <h3>Service 1</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, hic error. At, cupiditate
+                            inventore. Non, ullam quibusdam. Alias repudiandae quidem numquam temporibus dolorum ab
+                            accusantium, accusamus, totam perspiciatis voluptatum consequatur.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fa fa-handshake"></i>
+                        <h3>Service 2</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, hic error. At, cupiditate
+                            inventore. Non, ullam quibusdam. Alias repudiandae quidem numquam temporibus dolorum ab
+                            accusantium, accusamus, totam perspiciatis voluptatum consequatur.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fa fa-building"></i>
+                        <h3>Service 3</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, hic error. At, cupiditate
+                            inventore. Non, ullam quibusdam. Alias repudiandae quidem numquam temporibus dolorum ab
+                            accusantium, accusamus, totam perspiciatis voluptatum consequatur.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-</div>
-<div class="second-container">
-  <h2 class="section-header">Exploring New Horizons</h2>
-    <p>
-      As a dedicated professional with a passion for innovation and excellence, I've embarked on a transformative journey in the realms of insurance and real estate. With a profound commitment to delivering exceptional value, I've led as a CEO, navigating the intricate landscape of these industries.
-    </p>
-    <p>
-      My role as a podcaster allows me to share insights, narratives, and thought-provoking discussions on a range of topics. This platform serves as an avenue to connect, inspire, and engage with a diverse audience, fostering meaningful conversations that resonate with our shared experiences.
-    </p>
-    <p>
-     Beyond the boardroom and the microphone, I thrive on exploring the synergy between business ventures, property endeavors, and the world of podcasts. This convergence creates a dynamic and vibrant tapestry, offering a unique blend of perspectives and opportunities.
-    </p>
-</div>
-<div class="third-container">
-  <h2>About</h2>
-  <p style="margin:0">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui tenetur dolorum harum aperiam ratione pariatur quo impedit, sint iure nihil eius repudiandae voluptatem labore dolor vitae omnis inventore cum perspiciatis quis? Modi mollitia ipsa expedita nostrum voluptate suscipit eius vel, tenetur aliquid, enim provident iste velit culpa quibusdam ad saepe, est eligendi ducimus? Autem, eum?
-  </p>
-</div>
+    </section>
+    <section class="section-container">
+        <h2 class="section-header">Exploring New Horizons</h2>
+        <p>
+            As a dedicated professional with a passion for innovation and excellence, I've embarked on a transformative
+            journey in the realms of insurance and real estate. With a profound commitment to delivering exceptional
+            value, I've led as a CEO, navigating the intricate landscape of these industries.
+        </p>
+        <p>
+            My role as a podcaster allows me to share insights, narratives, and thought-provoking discussions on a range
+            of topics. This platform serves as an avenue to connect, inspire, and engage with a diverse audience,
+            fostering meaningful conversations that resonate with our shared experiences.
+        </p>
+        <p>
+            Beyond the boardroom and the microphone, I thrive on exploring the synergy between business ventures,
+            property endeavors, and the world of podcasts. This convergence creates a dynamic and vibrant tapestry,
+            offering a unique blend of perspectives and opportunities.
+        </p>
+    </section>
+    <section class="section-container">
+        <h2 class="section-header">About</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui tenetur dolorum harum aperiam ratione pariatur
+            quo impedit, sint iure nihil eius repudiandae voluptatem labore dolor vitae omnis inventore cum perspiciatis
+            quis? Modi mollitia ipsa expedita nostrum voluptate suscipit eius vel, tenetur aliquid, enim provident iste
+            velit culpa quibusdam ad saepe, est eligendi ducimus? Autem, eum?
+        </p>
+    </section>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,58 +2,68 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}" />
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}"/>
     <title>Portfolio</title>
-<style>
-    .navbar-nav a.nav-item.nav-link {
+    <style>
+        .navbar-nav a.nav-item.nav-link {
             padding: 6px 15px;
             border-radius: 20px;
-            color: orange;
+            color: #FFDB58;
             margin: 0 10px;
             font-weight: 600;
         }
-    .navbar-nav a.nav-item.nav-link.active,
-    .navbar-nav a.nav-item.nav-link:hover {
-        background: orange;
-        color: white;
-        transition: color 0.3s;
-    }
-</style>
-    
+
+        .navbar-nav a.nav-item.nav-link.active,
+        .navbar-nav a.nav-item.nav-link:hover {
+            background: #FFDB58;
+            color: white;
+            transition: color 0.3s;
+        }
+    </style>
+
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #eef4ed;">
-        <a class="navbar-brand navbar-brand-orange" href="#" style="color:orange; font-weight: bold;">Shraddha </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #eef4ed;">
+    <a class="navbar-brand navbar-brand-orange" href="#" style="color:orange; font-weight: bold;">Shraddha </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link mx-3 {{ request()->is('/') ? 'active' : '' }}" href="/">Home</a>
-                <a class="nav-item nav-link mx-3 {{ request()->is('bio') ? 'active' : '' }}" href="{{ route('bio') }}">Bio</a>
-                <a class="nav-item nav-link mx-3 {{ request()->is('about') ? 'active' : '' }}" href="{{ route('about') }}">About</a>
-                <a class="nav-item nav-link mx-3 {{ request()->is('contact') ? 'active' : '' }}" href="{{ route('contact') }}">Contact</a>
-            </div>
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link mx-3 {{ request()->is('/') ? 'active' : '' }}" href="/">Home</a>
+            <a class="nav-item nav-link mx-3 {{ request()->is('bio') ? 'active' : '' }}"
+               href="{{ route('bio') }}">Bio</a>
+            <a class="nav-item nav-link mx-3 {{ request()->is('about') ? 'active' : '' }}" href="{{ route('about') }}">About</a>
+            <a class="nav-item nav-link mx-3 {{ request()->is('contact') ? 'active' : '' }}"
+               href="{{ route('contact') }}">Contact</a>
         </div>
-    </nav>
-    <div>
-        @yield('content')
     </div>
-    <footer class="footer">
+</nav>
+<main>
+    @yield('content')
+</main>
+<footer class="footer">
     <div class="container">
-    <div class="row">
+        <div class="row">
             <div class="col-md-6">
                 <h3>Contact Information</h3>
                 <p>Email: shraddhashrestha@gmail.com</p>
@@ -62,9 +72,11 @@
             <div class="col-md-6">
                 <h3>Follow Me</h3>
                 <ul class="social-media">
-                    <li><a href="https://www.facebook.com" class="social-link"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="https://www.facebook.com" class="social-link"><i class="fab fa-facebook-f"></i></a>
+                    </li>
                     <li><a href="https://www.twitter.com" class="social-link"><i class="fab fa-twitter"></i></a></li>
-                    <li><a href="https://www.instagram.com" class="social-link"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="https://www.instagram.com" class="social-link"><i class="fab fa-instagram"></i></a>
+                    </li>
                 </ul>
             </div>
         </div>
