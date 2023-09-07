7fea6e7b77236c0c4a831c6fa4bceffa81e60698
Sulav Shrestha <sulavstha2629@gmail.com>
2023-09-07 15:44:04 +0545

    Made About Page with Images Responsive.

--- resources/views/frontend/posts.blade.php
+++ resources/views/frontend/posts.blade.php
@@ -24,22 +24,13 @@
                     </div>
                 @endforeach
             </div>
-     <div id="blog-post-list" class="card-container">
-        @foreach ($posts as $post)
-        <div class="blog-card">
-            <p>{{ $post->datePosted }}</p>
-            <h3>{{ $post->title }}</h3>
-            <p class="story-content">{{ $post->post }}</p>
-            <a href="{{ route('viewfullpost',$post->id) }}"><span>Read More.....</span></a>
-        </div>
-        @endforeach
 
-            {{-- <div class="blog-card">
-                <p>6th August 2023</p>
-                <h3>Blog Post Title 1</h3>
-                <p>Short excerpt or summary of the blog post content goes here...</p>
-                <a href="blog-post-1.html"><span>Read More.....</span></a>
-            </div> --}}
-        </div>
+
+        {{-- <div class="blog-card">
+            <p>6th August 2023</p>
+            <h3>Blog Post Title 1</h3>
+            <p>Short excerpt or summary of the blog post content goes here...</p>
+            <a href="blog-post-1.html"><span>Read More.....</span></a>
+        </div> --}}
     </section>
 @endsection
